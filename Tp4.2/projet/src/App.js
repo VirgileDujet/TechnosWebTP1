591ebe3459d88d1074b48d154fa9734792c8c1d4
VirgileDujet <38044199+VirgileDujet@users.noreply.github.com>
2019-02-21 10:39:00 +0100

    Utilisation react

---
+++ Tp4.2/projet/src/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import Header from './container/Header';
+import SideMenu from './container/SideMenu';
+import Artist from './component/Artist';
+import AlbumGrid from './container/AlbumGrid';
+import {Container,Row,Col} from 'reactstrap';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Container>
+
+        <Row>
+          <Col>
+            <Header />
+          </Col>
+        </Row>
+
+        <Row>
+          <Col sm='2'>
+            <SideMenu />
+          </Col>
+          <Col sm='10'>
+            <main>
+              <Artist />
+              <AlbumGrid />
+            </main>
+          </Col>
+        </Row>
+
+      </Container>
+
+
+
+
+    );
+  }
+}
+
+export default App;
